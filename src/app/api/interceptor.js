44ebeb9641ab700de315d1dbf3da9b3277dcc883
abinesh-piccosoft-react <141411308+abinesh-piccosoft-react@users.noreply.github.com>
2025-01-08 17:03:05 +0530

    default key changes

--- src/app/api/interceptor.js
+++ src/app/api/interceptor.js
@@ -1,34 +1,30 @@
-export const apiinstance=async(url,options)=>{
-    const headers = {
-        'Content-Type': 'application/json',
-        "Authorization":process.env.NEXT_PUBLIC_SPURTCMS_NEXTJS_STARTER_THEME_TOKEN,
-        // "ApiKey":process.env.NEXT_PUBLIC_SPURTCMS_NEXTJS_STARTER_APIKEY
-        "ApiKey":process.env.next_public_spurtcms_nextjs_starter_apikey
-      }
-    
-    //   if (token) {
-    //     headers['Authorization'] = token
-    //   }
-    
-      const config = {
-        method: options.method || 'GET',
-        headers,
-        ...options,
-      }
-    
-      if (config.method === 'GET') {
-        delete config.body
-      } else {
-        config.body = config.body
-      }
+export const apiinstance = async (url, options) => {
+  const key = process.env.NEXT_PUBLIC_SPURTCMS_NEXTJS_STARTER_APIKEY ? process.env.NEXT_PUBLIC_SPURTCMS_NEXTJS_STARTER_APIKEY : process.env.NEXT_PUBLIC_SPURTCMS_NEXTJS_STARTER_DEFAULT_APIKEY
+  const headers = {
+    'Content-Type': 'application/json',
+    "Authorization": process.env.NEXT_PUBLIC_SPURTCMS_NEXTJS_STARTER_THEME_TOKEN,
+    "ApiKey": key
+  }
 
-  
-        const res = await fetch(`${process.env.NEXT_PUBLIC_SPURTCMS_NEXTJS_STARTER_THEME_BASEURL}${url}`,config);
-  
-        if(res.ok){
-            return await res.json();
-        }else{
-         
-        }
-     
+  const config = {
+    method: options.method || 'GET',
+    headers,
+    ...options,
+  }
+
+  if (config.method === 'GET') {
+    delete config.body
+  } else {
+    config.body = config.body
+  }
+
+  try {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_SPURTCMS_NEXTJS_STARTER_THEME_BASEURL}${url}`, config);
+    const data = await res.json()
+    return data
+  } catch (error) {
+    console.log(error)
+  }
+
+
 }
